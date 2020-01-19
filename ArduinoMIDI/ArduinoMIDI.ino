e13eafd7b82628d1a0bc58db1325efb24b9742d3
khamm004 <38026881+khamm004@users.noreply.github.com>
2020-01-19 07:43:47 -0800

    Update ArduinoMIDI.ino
    Added functionality for octave adjusting with joystick and visual output onto LED Bar.

--- ArduinoMIDI/ArduinoMIDI.ino
+++ ArduinoMIDI/ArduinoMIDI.ino
@@ -9,15 +9,27 @@
 
 */
 
-const byte ROWS = 3;
-const byte COLS = 4;
-
+ const byte ROWS = 3;
+ const byte COLS = 4;
+ int octave[] = {13,12,11,10,9};
+ 
+ int joyPin1 = 0;                 // slider variable connecetd to analog pin 0
+ int joyPin2 = 1;                 // slider variable connecetd to analog pin 1
+ const int midVal = 52;           // constant for value of specific joystick at center
+ bool isCentered = true;
+ int value1 = 0;                  // variable to read the value from the analog pin 0
+ int value2 = 0;                  // variable to read the value from the analog pin 1
+ int curLED = 2;
+ 
 // how the keypad has its keys laid out
-const char keys[ROWS][COLS] = {
-  {MIDI_F + MIDI_SHARP, MIDI_G + MIDI_SHARP, MIDI_A + MIDI_SHARP, MIDI_C + MIDI_SHARP},
-  {MIDI_D + MIDI_SHARP, MIDI_E, MIDI_D, MIDI_C},
-  {MIDI_B, MIDI_A, MIDI_G, MIDI_F},
+/*const*/ char keys[ROWS][COLS] = {
+  {MIDI_C + MIDI_SHARP, MIDI_D + MIDI_SHARP, MIDI_F + MIDI_SHARP, MIDI_G + MIDI_SHARP},
+  {MIDI_A + MIDI_SHARP, MIDI_B, MIDI_A, MIDI_G},
+  {MIDI_F, MIDI_E, MIDI_D, MIDI_C},
 };
+  /*{MIDI_F + MIDI_SHARP, MIDI_G + MIDI_SHARP, MIDI_A + MIDI_SHARP, MIDI_C + MIDI_SHARP},
+  {MIDI_D + MIDI_SHARP, MIDI_E, MIDI_D, MIDI_C},
+  {MIDI_B, MIDI_A, MIDI_G, MIDI_F},*/
 
 const byte rowPins[ROWS] = {6, 7, 8}; //connect to the row pinouts of the keypad
 const byte colPins[COLS] = {2, 3, 4, 5}; //connect to the column pinouts of the keypad
@@ -26,18 +38,64 @@ Keypad_Matrix kpd = Keypad_Matrix( makeKeymap (keys), rowPins, colPins, ROWS, CO
 
 void keyDown (const char which)
 {
-  midi_note_on(0, which, 0xff);
+  midi_note_on(0, which, 100);
 }
 
 void keyUp (const char which)
 {
-  midi_note_off(0, which, 0xff);
+  midi_note_off(0, which, 100);
 }
 
+void check_X_Direct()
+{
+  value1 = analogRead(joyPin1);
+  
+  if((treatValue(value1)< midVal) && (keys[1][3] > 48) && (isCentered == true)){
+    isCentered = false;
+    digitalWrite(octave[curLED], LOW);
+    for (int i = 0; i < ROWS; ++i){
+      for(int j = 0; j < COLS; ++j){
+        keys[i][j] = keys[i][j] - MIDI_OCTAVE;
+      }
+    }
+    curLED = curLED - 1;
+    digitalWrite(octave[curLED],HIGH);
+  }
+  else if((treatValue(value1)> midVal) && (keys[1][3] < 84) && (isCentered == true)){
+    isCentered = false;
+    digitalWrite(octave[curLED],LOW);
+    for (int i = 0; i < ROWS; ++i){
+      for(int j = 0; j < COLS; ++j){
+        keys[i][j] = keys[i][j] + MIDI_OCTAVE;
+      }
+    }
+    curLED = curLED + 1;
+    digitalWrite(octave[curLED],HIGH);
+  }
+  else{
+    isCentered = true;
+  }
+}
+
+void check_Y_Direct()
+{
+  value2 = analogRead(joyPin2);
+    if(treatValue(value2)< midVal){
+    
+    }
+    else if(treatValue(value2)> midVal){
+    
+    } 
+}
 
 void setup() 
 {
-  pinMode(9, OUTPUT);
+  pinMode(octave[0], OUTPUT);         // initializes digital pins 0 to 7 as outputs
+  pinMode(octave[1], OUTPUT);
+  pinMode(octave[2], OUTPUT);
+  pinMode(octave[3], OUTPUT);
+  pinMode(octave[4], OUTPUT);
+  
   Serial.begin (115200);
   Serial.println ("Starting.");
   kpd.begin ();
@@ -45,7 +103,26 @@ void setup()
   kpd.setKeyUpHandler   (keyUp);
 }
 
+int treatValue(int data) {
+  return (data * 9 / 1024) + 48;
+ }
+ 
 void loop() 
 {
   kpd.scan ();
+  digitalWrite(octave[curLED], HIGH);
+
+  check_X_Direct();
+  
+  delay(100);            
+  // reads the value of the variable resistor 
+  check_Y_Direct();
+  
+  /*digitalWrite(ledPin, HIGH);          
+  delay(value1);
+  digitalWrite(ledPin, LOW);
+  delay(value2);
+  Serial.print('J');
+  Serial.print(treatValue(value1));
+  Serial.println(treatValue(value2));*/
 }
